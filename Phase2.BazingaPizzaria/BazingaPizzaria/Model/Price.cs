c511de913498bce310c3dee25ccc1c480c3aad04
John Velis <john@johnvelis.com>
2015-02-23 15:54:09 -0500

    ST01

---
+++ Phase2.BazingaPizzaria/BazingaPizzaria/Model/Price.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazingaPizzaria.Model
+{
+    class Price
+    {
+            {
+        #region Fields
+        private Double _beverageSm;
+        private Double _beverageMd;
+        private Double _beverageLg;
+
+        #endregion
+
+        #region Properties
+        public Double BeverageSm
+        {
+            get { return _beverageSm; }
+        }
+
+        public Double BeverageMd
+        {
+            get { return _beverageMd; }
+        }
+        public Double BeverageLg
+        {
+            get { return _beverageLg; }
+        }
+
+        #endregion
+
+        #region Constructors
+        Price()
+        {
+            _beverageSm = 1.19;
+            _beverageMd = 1.69;
+            _beverageLg = 2.09;
+        }
+        #endregion
+    }
+}
