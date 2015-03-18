eafe4fb9d1cebedde5d8ae639d761928eed008d4
John Velis <john@johnvelis.com>
2015-03-18 14:42:08 -0400

    update GenerateTestData 2:48

--- Phase2.BazingaPizzaria/BazingaPizzaria/Model/GenerateTestData.cs
+++ Phase2.BazingaPizzaria/BazingaPizzaria/Model/GenerateTestData.cs
@@ -10,6 +10,9 @@ namespace BazingaPizzaria.Model
     {
         internal static Order GenerateTestOrderData()
         {
+            //
+            // generate test order
+            //
             Order testOrder = new Order();
             testOrder.LastName = "Flintstone";
             testOrder.FirstName = "Fred";
@@ -24,6 +27,39 @@ namespace BazingaPizzaria.Model
             testOrder.CCExpMonth = 5;
             testOrder.CCExpYear = 2017;
             testOrder.CCCVVCode = 345;
+
+            //
+            // genearate pizza for order with toppings
+            //
+            Pizza pizzaOne = new Pizza();
+            pizzaOne.PizzaID = 0001;
+            pizzaOne.Size = "Large";
+            pizzaOne.Crust = "Garlic";
+            pizzaOne.IsSpecialty = false;
+            pizzaOne.Quantity = 1;
+
+            //
+            // add toppings to pizzaOne
+            //
+            Pizza.Topping firstTopping = Pizza.Topping.Pepperoni;
+            Pizza.Topping secondTopping = Pizza.Topping.Ham;
+            Pizza.Topping thirdTopping = Pizza.Topping.ItalianSausage;
+            pizzaOne.PizzaToppings.Add(firstTopping);
+            pizzaOne.PizzaToppings.Add(secondTopping);
+            pizzaOne.PizzaToppings.Add(thirdTopping);
+
+            //
+            // genearate specialty pizza for order (2 pizzas)
+            //
+            Pizza pizzaTwo = new Pizza();
+            pizzaOne.PizzaID = 0002;
+            pizzaOne.Size = "Small";
+            pizzaOne.Crust = "Cheesy";
+            pizzaOne.IsSpecialty = true;
+            pizzaOne.Quantity = 2;
+
+            //
+            // add pizzaOne and pizzaTwo to testOrder
 
             return testOrder;
         }
