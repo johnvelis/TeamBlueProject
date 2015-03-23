422daee56ff7bc81c6c14dbe6270c56af1d28c43
John Velis <john@johnvelis.com>
2015-03-23 14:27:25 -0400

    john's change

--- Phase2.BazingaPizzaria/BazingaPizzaria/FormMain.cs
+++ Phase2.BazingaPizzaria/BazingaPizzaria/FormMain.cs
@@ -459,6 +459,8 @@ namespace BazingaPizzaria
                     dataGridView1.Rows.Add(newBeverage.Quantity, newBeverage.SizeName, newBeverage.BevPrice);
 
                     dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+
+                    newOrder.BeveragePurchase.Add(newBeverage);
                 }
             }
 
@@ -594,12 +596,14 @@ namespace BazingaPizzaria
             //
             // fill in the Pizzas Information GroupBox before order confirmation
             //
+            listBox_JV_Pizzas.DataSource = null;
             listBox_JV_Pizzas.DataSource = newOrder.PizzaPurchase;
             textBox_JV_PizzaSubtotal.Text = newOrder.PizzaSubtotal().ToString("C");
 
             //
             // fill in the Beverage Information GroupBox before order confirmation
             //
+            listBox_JV_Beverages.DataSource = null;
             listBox_JV_Beverages.DataSource = newOrder.BeveragePurchase;
             textBox_JV_BeverageSubtotal.Text = newOrder.BeverageSubtotal().ToString("C");
 
